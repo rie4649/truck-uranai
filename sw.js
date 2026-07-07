83acd27955666daee94d1bef9e96d6768f7287ee
rie4649 <okm.co@icloud.com>
2026-07-07 18:44:15 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,6 @@
+// このアプリを「ホーム画面に追加(インストール)」できるようにするための
+// 最小限のサービスワーカーです。オフラインキャッシュ等は行いません。
+self.addEventListener("install", (e) => { self.skipWaiting(); });
+self.addEventListener("activate", (e) => { self.clients.claim(); });
+self.addEventListener("fetch", (e) => { /* 素通し */ });
 
